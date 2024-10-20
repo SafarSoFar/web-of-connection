5376daceea6ec6717351dc940dc39e5d9748dc9b
Isaev Safar <133248217+SafarSoFar@users.noreply.github.com>
2024-10-20 09:28:07 -0700

    ASCII effect, debug menu toggling on 'h'

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ import {GUI} from 'lil-gui';
 
 const scene = new THREE.Scene(); 
 
-const dotGeometry = new THREE.SphereGeometry(1); 
+const dotGeometry = new THREE.SphereGeometry(5); 
 const dotMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } ); 
 
 let dots = [];
@@ -24,6 +24,7 @@ const settings = {
      dotTraversalRange: 100,
      maxLineDistance: 30.0,
      dotsSpeed: 0.05,
+     deleteConnectionAfterDistanceLimit: false,
      deleteConnections: function(){
           for(let i = 0; i < connections.length; i++){
                scene.remove(connections[i].line);
@@ -64,6 +65,7 @@ gui.add(settings, 'dotsAmount', 3, 30, 1).onChange(value => changeDotsAmount(val
 gui.add(settings, 'dotTraversalRange', 30, 100);
 gui.add(settings, 'maxLineDistance', 15, 100);
 gui.add(settings, 'dotsSpeed', 0.05, 1);
+gui.add(settings, 'deleteConnectionAfterDistanceLimit');
 gui.add(settings, 'deleteConnections');
 
 
@@ -119,20 +121,28 @@ const renderer = new THREE.WebGLRenderer();
 // renderer.setClearColor(0xffffff);
 renderer.setSize( window.innerWidth, window.innerHeight ); 
 
-// let effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: true, alpha: true} );
-// effect.setSize( window.innerWidth, window.innerHeight );
-// effect.domElement.style.color = 'white';
-// effect.domElement.style.backgroundColor = 'black';
+let effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: true, alpha: true} );
+effect.setSize( window.innerWidth, window.innerHeight );
+effect.domElement.style.color = 'white';
+effect.domElement.style.backgroundColor = 'black';
 
 // Special case: append effect.domElement, instead of renderer.domElement.
 // AsciiEffect creates a custom domElement (a div container) where the ASCII elements are placed.
 
-// document.body.appendChild( effect.domElement );
+document.body.appendChild( effect.domElement );
 
-document.body.appendChild( renderer.domElement );
+// document.body.appendChild( renderer.domElement );
 
-// const controls = new OrbitControls(camera, effect.domElement );
-const controls = new OrbitControls(camera, renderer.domElement );
+const controls = new OrbitControls(camera, effect.domElement );
+controls.update();
+// const controls = new OrbitControls(camera, renderer.domElement );
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 72) { // 'h' ascii value
+        gui.show(gui._hidden);
+    }
+};
 
 
 for(let i = 0; i < settings.dotsAmount; i++){
@@ -165,9 +175,13 @@ function animate() {
           }
      }
      
-     // effect.render( scene, camera ); 
-     renderer.render( scene, camera ); 
      
+     effect.render( scene, camera ); 
+     // renderer.render( scene, camera ); 
+
+     if(settings.deleteConnectionAfterDistanceLimit){
+          settings.deleteConnections();
+     }
 } 
 
 renderer.setAnimationLoop( animate );
